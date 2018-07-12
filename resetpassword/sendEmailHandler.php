28a93f08fc85ed417191935739ddcca4b1758ebb
Brody Downs <brodydowns@u.boisestate.edu>
2018-07-12 13:56:18 -0600

    HTML Email configuration

--- resetpassword/sendEmailHandler.php
+++ resetpassword/sendEmailHandler.php
@@ -13,6 +13,7 @@ if(isset($userInfo['email'])) {
   $key = $_POST['email'] . time() . rand();
   $hash = password_hash($key, PASSWORD_BCRYPT);
   $hash = substr($hash, 7, strlen($hash));
+  $link = "http://internetcatdatabase.herokuapp.com/resetpassword?key=" . $hash;
 
   //echo "internetcatdatabase.herokuapp.com/resetpassword?key=" . $hash;
 
@@ -25,28 +26,34 @@ if(isset($userInfo['email'])) {
   }
 
   $dao->insertResetPasswordHash($userInfo['id'], $hash);
+
 //send email
+$message = "Hello " . $userInfo['username'] . ",
+<br > <br >
+Click the following link to reset password, it will only be available for a limited amount of time:
+<br > <br >
+<a href='" . $link . "'>" . $link . "</a>";
+
 $from = new SendGrid\Email(null, "icatsdb@gmail.com");
 $subject = "ICDB Reset Password Link";
 $to = new SendGrid\Email(null, $_POST['email']);
-$content = new SendGrid\Content("text/html", "Hello " . $userInfo['username'] . ",
-<br >
-Click the following link to reset password, it will only be available for a limited amount of time:
-<br >
-internetcatdatabase.herokuapp.com/resetpassword?key=" . $hash);
+$content = new SendGrid\Content("text/html", $message);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
+
+/*
 echo $response->statusCode();
 echo "<br > <br >";
 echo $response->headers();
 echo "<br > <br >";
 echo $response->body();
-
-
+echo "<br > <br >";
+echo $message;
+*/
 
 $_SESSION['successMessage'] = "An email has been sent to " . $_POST[email];
 header("Location:../resetpassword");
